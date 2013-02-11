f09af39c8783abc5d9e7e2fc796f3d265ad6c875
miketihonchik <mike.tihonchik@gmail.com>
2013-02-11 17:38:22 -0600

    add CalendarView with ListView for "calendar" screen; bumped minimum API version to 11 (from previous 8)

--- myiclubonline/src/com/abcfinancial/android/myiclubonline/user/CalendarActivity.java
+++ myiclubonline/src/com/abcfinancial/android/myiclubonline/user/CalendarActivity.java
@@ -4,10 +4,29 @@ import com.abcfinancial.android.myiclubonline.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.ListView;
 
 public class CalendarActivity extends Activity {
-	   public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.tab_activity_calendar);
-	    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tab_activity_calendar);
+
+		CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView1);
+		calendarView.setOnDateChangeListener(new OnDateChangeListener() {
+			public void onSelectedDayChange(CalendarView view, int year,
+					int month, int dayOfMonth) {
+
+			}
+		});
+		
+		ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                new String[]{"One","Two"}){
+
+        });
+	}
 }
