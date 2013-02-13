82993f1345e091722e96602e867e244c35903020
miketihonchik <mike.tihonchik@gmail.com>
2013-02-13 13:35:08 -0600

    add "personal info" screen

--- myiclubonline/src/com/abcfinancial/android/myiclubonline/user/agreement/AgreementActivity.java
+++ myiclubonline/src/com/abcfinancial/android/myiclubonline/user/agreement/AgreementActivity.java
@@ -1,8 +1,13 @@
 package com.abcfinancial.android.myiclubonline.user.agreement;
 
+import org.json.JSONObject;
+
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,33 +15,75 @@ import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 
 import com.abcfinancial.android.myiclubonline.R;
+import com.abcfinancial.android.myiclubonline.common.RequestMethod;
+import com.abcfinancial.android.myiclubonline.common.WebServiceClient;
 import com.abcfinancial.android.myiclubonline.usergroups.AccountGroupActivity;
 
 public class AgreementActivity extends ListActivity {
-	static final String[] ACTIONS = new String[] { "Agreement Info", "Personal Info",
-			"Club Info" };
-
+	static final String[] ACTIONS = new String[] { "Agreement Info", "Personal Info", "Club Info" };
+	private String memberId = "", memberNumber = "", club;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setListAdapter(new ArrayAdapter<String>(this, R.layout.list_simple, ACTIONS));
 
-		ListView listView = getListView();
-		listView.setTextFilterEnabled(true);
-
-		listView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				if (position == 0) {
+		try {
+			SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+			String mobileLoginMember = preferences.getString("mobileLoginMember", "");
+			memberId = preferences.getString("memberId", "");
+			JSONObject loginMember = new JSONObject(mobileLoginMember);
+			club = loginMember.getString("homeClub");
+			memberNumber = club + loginMember.getString("memberNumber");
+			
+			ListView listView = getListView();
+			listView.setTextFilterEnabled(true);
+			listView.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					if (position == 0) {
+					}
+					if (position == 1) {
+						new WebServiceTask().execute("personallookup", memberId, memberNumber, club);
+					}
+					if (position == 2) {
+						Intent intent = new Intent(getParent(), ClubInfoActivity.class);
+						AccountGroupActivity parentActivity = (AccountGroupActivity) getParent();
+						parentActivity.startChildActivity("ClubInfoActivity", intent);
+					}
 				}
-				if (position == 1) {
-				}
-				if (position == 2) {
-					Intent intent = new Intent(getParent(), ClubInfoActivity.class);
-					AccountGroupActivity parentActivity = (AccountGroupActivity) getParent();
-					parentActivity.startChildActivity(ACTIONS[2], intent);
-				}
-			}
-		});
+			});
+		} catch (Exception exception) {
+			exception.printStackTrace();
+			// TODO: throw error dialog
+		}		
 	}
+	
+	private class WebServiceTask extends AsyncTask<String, String, String> {
+
+		@Override
+		protected String doInBackground(String... uri) {
+			WebServiceClient client = new WebServiceClient(uri[0]);
+			try {
+				if("".equals(uri[1])){
+					client.addParameter("memberNumber", uri[2]);
+				} else {
+					client.addParameter("memberId", uri[1]);
+				}
+				client.addParameter("club", uri[3]);
+				client.addHeader("Authorization", "Basic cWE6dGVzdA==");
+				client.execute(RequestMethod.POST);
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+			return client.getResponse();
+		}
+
+		@Override
+		protected void onPostExecute(String response) {
+			Intent intent = new Intent(getParent(), PersonalInfoActivity.class);
+			intent.putExtra("PERSONAL", response);
+			AccountGroupActivity parentActivity = (AccountGroupActivity) getParent();
+			parentActivity.startChildActivity("PersonalInfoActivity", intent);
+		}
+	}	
 }
---
+++ myiclubonline/src/com/abcfinancial/android/myiclubonline/user/agreement/PersonalInfoActivity.java
@@ -0,0 +1,74 @@
+package com.abcfinancial.android.myiclubonline.user.agreement;
+
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.abcfinancial.android.myiclubonline.R;
+
+public class PersonalInfoActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.account_personalinfo);
+
+		try {
+			Bundle extras = getIntent().getExtras();
+			String personal = extras.getString("PERSONAL");
+			System.out.println("RESPONSE: " + personal);
+			JSONObject personalInfo = new JSONObject(personal);
+			JSONObject personalData = personalInfo.getJSONObject("personal");
+			
+			String fullName = personalData.getString("firstName") + personalData.getString("lastName");
+			String addressValue = "", address = personalData.getString("address");
+			String cityValue = "", city = personalData.getString("city");
+			String stateValue = "", state = personalData.getString("state");
+			String zipValue = "", zip = personalData.getString("zip");
+			if( address != null && !"".equals(address)) {
+				addressValue = address;
+			}
+			if( city != null && !"".equals(city)) {
+				cityValue = city;
+			}
+			if( state != null && !"".equals(state)) {
+				stateValue = ", " + state;
+			}
+			if( zip != null && !"".equals(zip)) {
+				zipValue = " " + zip;
+			}
+			String cityStateZip = cityValue + stateValue + zipValue;
+
+			String homePhoneValue = "", mobilePhoneValue = "", homePhone = personalData.getString("homePhone"), mobilePhone = personalData.getString("cellPhone");
+			if( homePhone!= null && !"".equals(homePhone) && !"0000000000".equals(homePhone) ) {
+				homePhoneValue = homePhone;
+			}
+			if( mobilePhone!= null && !"".equals(homePhone) && !"0000000000".equals(mobilePhone) ) {
+				mobilePhoneValue = mobilePhone;
+			}
+			String emailValue = "", email = personalData.getString("email");
+			if( email != null && !"".equals(email)) {
+				emailValue = email;
+			}
+
+			TextView fullNameLine = (TextView) findViewById(R.id.fullName);
+			TextView addressLine = (TextView) findViewById(R.id.address);
+			TextView cityLine = (TextView) findViewById(R.id.cityStateZip);
+			TextView homePhoneLine = (TextView) findViewById(R.id.homePhone);
+			TextView mobilePhoneLine = (TextView) findViewById(R.id.mobilePhone);
+			TextView emailLine = (TextView) findViewById(R.id.email);
+
+			fullNameLine.setText(fullName);
+			addressLine.setText(addressValue);
+			cityLine.setText(cityStateZip);
+			homePhoneLine.setText(homePhoneValue);
+			mobilePhoneLine.setText(mobilePhoneValue);
+			emailLine.setText(emailValue);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+			// TODO: throw error dialog
+		}
+	}
+}
