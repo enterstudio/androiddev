bc21838720a9bf0271aaa6d4738d35561396a36a
miketihonchik <mike.tihonchik@gmail.com>
2014-01-13 12:55:30 -0600

    finish up design of the main screen

--- lenonhonor360/src/com/tihonchik/lenonhonor360/ui/user/MainActivity.java
+++ lenonhonor360/src/com/tihonchik/lenonhonor360/ui/user/MainActivity.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.tihonchik.lenonhonor360.ui.BaseActivity;
