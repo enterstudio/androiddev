5a4831724a856c576ab07ba035f11bdc448b4620
miketihonchik <mike.tihonchik@gmail.com>
2014-01-13 11:16:09 -0600

    design for new blog post

--- lenonhonor360/src/com/tihonchik/lenonhonor360/ui/user/MainActivity.java
+++ lenonhonor360/src/com/tihonchik/lenonhonor360/ui/user/MainActivity.java
@@ -1,44 +1,52 @@
 package com.tihonchik.lenonhonor360.ui.user;
 
+import java.io.IOException;
+
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-import com.tihonchik.lenonhonor360.R;
 import com.tihonchik.lenonhonor360.ui.BaseActivity;
+import com.tihonchik.lenonhonor360.util.AppUtils;
+import com.tihonchik.lenonhonor360.R;
 
 public class MainActivity extends BaseActivity {
 
-    @Override
-    protected Context getContextforBase() {
-        return MainActivity.this;
-    }
+	@Override
+	protected Context getContextforBase() {
+		return MainActivity.this;
+	}
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+	}
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().setBackgroundDrawable(null);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        getWindow().setBackgroundDrawable(null);
+		ImageView newBlogImage = (ImageView) findViewById(R.id.new_blog_image);
+		try {
+			int sdk = android.os.Build.VERSION.SDK_INT;
+			if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+				newBlogImage.setBackgroundDrawable(AppUtils
+						.getImageFromURL("http://placehold.it/320x240"));
+			} else {
+				newBlogImage.setBackground(AppUtils
+						.getImageFromURL("http://placehold.it/320x240"));
+			}
+		} catch (IOException exception) {
+			// do nothing
+		}
+	}
 
-        if(savedInstanceState == null) {
-/*            Fragment f = new UserLoginFragment();
-            getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.body, f, "UserLoginFragment")
-                .commit();
-*/        }
-    }
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
 
-    @Override
-    public void onPause() {
-    	super.onPause();
-    }
-    
 	@Override
 	protected int getContentLayoutId() {
 		return R.layout.activity_main;
