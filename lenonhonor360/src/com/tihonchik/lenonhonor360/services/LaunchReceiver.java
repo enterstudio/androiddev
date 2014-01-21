51c8d6870c7819e8dcf90e7abe515e7aebf95f12
miketihonchik <mike.tihonchik@gmail.com>
2014-01-21 14:52:10 -0600

    refactor

---
+++ lenonhonor360/src/com/tihonchik/lenonhonor360/services/LaunchReceiver.java
@@ -0,0 +1,16 @@
+package com.tihonchik.lenonhonor360.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class LaunchReceiver extends BroadcastReceiver {
+	public static final String ACTION_PULSE_ALARM = "com.tihonchik.lenonhonor360.ACTION_PULSE_ALARM";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		System.out.println("LH360 - onReceive()... start service...");
+		Intent serviceIntent = new Intent(context, BlogPullService.class);
+		context.startService(serviceIntent);
+	}
+}
