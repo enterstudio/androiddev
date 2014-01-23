13f9ec670bd9e785337302b572bfb55e1540eaaa
miketihonchik <mike.tihonchik@gmail.com>
2014-01-23 12:33:43 -0600

    load new/updated graphics; add official URLs

--- lenonhonor360/src/com/tihonchik/lenonhonor360/AppConfig.java
+++ lenonhonor360/src/com/tihonchik/lenonhonor360/AppConfig.java
@@ -3,6 +3,7 @@ package com.tihonchik.lenonhonor360;
 public class AppConfig {
     public static final int SPLASHSCREENDELAY = 10000;
     
-    public static final String OFFICIAL_WEBSITE_URL = "";
-    public static final String OFFICIAL_APPSITE_URL = "";
+    public static final String OFFICIAL_WEBSITE_URL = "lenonhonor360.com";
+    public static final String OFFICIAL_APPSITE_URL = "equinetsolutions.com/lenonhonor360/blogpage.html";
+    public static final String OFFICIAL_APPSITE_PHP_URL = "www.equinetsolutions.com/lenonhonor360/preview.php";
 }
